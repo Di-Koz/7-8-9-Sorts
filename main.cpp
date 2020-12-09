1f5fce895c8e5911f2c70411ab29d7699a687324
Di-Koz <73629322+Di-Koz@users.noreply.github.com>
2020-12-09 19:54:26 +0700

    Updated file

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <cstring>    // memcpy
 #include <ctime>      // clock
 #include <cstdlib>    // rand
+#include <string>    // rand
+#include <fstream>    // files
+#include <vector>    // vector
 
 using namespace std;
 
@@ -10,8 +13,7 @@ inline void swap(int& a, int& b)
 {
     int tmp = a; a = b; b = tmp;
 }
-
-
+//сортировка обменом
 void sort_bubble(int* arr, size_t size)
 {
     int dlina = size, ind{ 0 };
@@ -24,12 +26,8 @@ void sort_bubble(int* arr, size_t size)
         }
         ++ind;
     }
-    /*for (int i{ 0 }; i < dlina; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;*/
 }
-
+//шейкер-сортировка
 void sort_shaker(int* arr, size_t size)
 {
     int dlina = size, indLeft{ 0 }, indRight{dlina - 1};
@@ -57,12 +55,8 @@ void sort_shaker(int* arr, size_t size)
             ++indLeft;
         }
     }
-    /*for (int i{ 0 }; i < dlina; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;*/
 }
-
+//сортировка выбором
 void sort_select(int* arr, size_t size)
 {
     int dlina = size, ind{ 0 };
@@ -79,12 +73,8 @@ void sort_select(int* arr, size_t size)
         swap(arr[ind], arr[minInd]);
         ++ind;
     }
-    /*for (int i{ 0 }; i < dlina; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;*/
 }
-
+//сортировка вставками
 void sort_insert_lin(int* arr, size_t size)
 {
     int dlina = size;
@@ -103,45 +93,250 @@ void sort_insert_lin(int* arr, size_t size)
         //после всех перемещений вставляю на нужное место
         arr[ind] = ch;
     }
-    /*for (int i{0}; i < dlina; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;*/
 }
-
-void sort_insert_bin(int* arr, size_t size)
-{
-
-}
-
+//сортировка Шелла (шаг = n / 2)
 void sort_shell_div2(int* arr, size_t size)
 {
+    int dlina = size, step{ dlina / 2 };
 
+    while (step >= 1) {
+        for (int i{ 0 }; i < step; ++i) {
+            int mainInd{ i }, ind{ mainInd + step };
+
+            while (ind < dlina) {
+                int ch = arr[ind];
+                int prevInd = ind - step;
+
+                //пока не дошли до начала отсортированной части и если
+                //какой-то из предыдущих больше, то перезаписываем
+                while ((prevInd >= mainInd) && (arr[prevInd] > ch)) {
+                    arr[prevInd + step] = arr[prevInd];
+                    prevInd -= step;
+                }
+
+                arr[prevInd + step] = ch;
+                ind += step;
+            }
+        }
+        step /= 2;
+    }
 }
-
+//сортировка Шелла(шаг = n * 3 + 1)
 void sort_shell_pow3(int* arr, size_t size)
 {
+    int dlina = size, step{1};
+    //выбор шага
+    while (step < dlina / 3) {
+        step = 3 * step + 1;
+    }
+    
+    while (step >= 1) {
+        for (int i{0}; i < step; ++i) {
+            int mainInd{ i }, ind{ mainInd + step };
 
+            while (ind < dlina) {
+                int ch = arr[ind];
+                int prevInd = ind - step;
+                
+                //пока не дошли до начала отсортированной части и если
+                //какой-то из предыдущих больше, то перезаписываем
+                while ((prevInd >= mainInd) && (arr[prevInd] > ch)) {
+                    arr[prevInd + step] = arr[prevInd];
+                    prevInd -= step;
+                }
+
+                arr[prevInd + step] = ch;
+                ind += step;
+            }
+        }
+        step = (step - 1) / 3;
+    }
 }
-
+//просеивание
+void sift(int* arr, int indEl, int indLast) {
+    while (indEl * 2 <= indLast) {
+        //предполагаем, что сравнивать будем с левым потомком
+        int ind{ indEl * 2 };
+        //если левый потомок это не последний элемент (иначе выйдем за 
+        //границы массива) и если левый потомок меньше правого потомка, 
+        //то сравнивать будем с правым потомком
+        if ((ind <= indLast - 1) && (arr[ind] < arr[ind + 1])) {
+            ++ind;
+        }
+        //если элемент меньше того потомка, с которым сравниваем, меняем их
+        if (arr[indEl] < arr[ind]) {
+            swap(arr[indEl], arr[ind]);
+            indEl = ind;
+        }
+        //если нет, остановить просеивание 
+        else {
+            return;
+        }
+    }
+}
+//пирамидальная сортировка
 void sort_heap(int* arr, size_t size)
 {
+    int dlina = size, halfDlina{ (dlina / 2) - 1 }; 
 
+    //просеивание от эл-та (dlina / 2) - 1 до начала массива
+    while (halfDlina >= 0) {
+        sift(arr, halfDlina, dlina - 1);
+        --halfDlina;
+    }
+
+    int lastInd{ dlina - 1 };
+    //идем до 1 элемента
+    while (lastInd > 0) {
+        swap(arr[0], arr[lastInd]);
+        --lastInd;
+        sift(arr, 0, lastInd);
+    }
 }
-
-void sort_quick_rec(int* arr, size_t size)
+//быстрая сортировка(рекурсия)
+void sort_quick_rec(int* arr, int size, int indLeft, int indRight)
 {
+    //допустим, пилотируемый элемент самый левый
+    int left{indLeft}, right{indRight}, ch{ arr[left] };
+    
+    do {
+        while (arr[left] < ch) {
+            ++left;
+        }
 
+        while (arr[right] > ch) {
+            --right;
+        }
+
+        if (left <= right) {
+            if (left != right) {
+                swap(arr[left], arr[right]);
+            }
+            ++left;
+            --right;
+        }
+    } while (left <= right);
+    //спуск
+    if (left < indRight) {
+        sort_quick_rec(arr, size, left, indRight);
+    }   
+    if (indLeft < right) {
+        sort_quick_rec(arr, size, indLeft, right);
+    }
 }
-
+//разделение и получение медианы
+int separAndGetMed(int* arr, int indL, int indR) {
+    int left = indL, right = indR, ch{ arr[left] };
+    //допустим пилотируемый элемент самый левый
+    //разделение
+    while (left < right) {
+        while ((arr[right] >= ch) && (left < right)) {
+            right--;
+        }
+        if (left != right) {
+            arr[left] = arr[right];
+            left++;
+        }
+        while ((arr[left] <= ch) && (left < right)) {
+            left++;
+        }
+        if (left != right) {
+            arr[right] = arr[left];
+            right--;
+        }
+    }
+    //пилотируемый элемент вставляем на его место
+    arr[left] = ch;
+    //возвращаем медиану
+    return left;
+}
+//быстрая сортировка(не рекурсия)
 void sort_quick_nonrec(int* arr, size_t size)
 {
+    int dlina = size, ind{ 0 };
+    //создаем вектор(чтобы сохранить нервы) медиан, 
+    //по которому будем перемещаться и добавлять элементы
+    vector<int> medians = { 0, dlina - 1 };
 
+    while (ind < dlina) {
+        //получаем медиану
+        int left = medians[ind], right = medians[ind + 1],
+            med = separAndGetMed(arr, left, right);
+        //если медиана совпала с левым индексом, значит элемент
+        //наименьший из этой последовательности
+        if (med == left) {
+            ++med;
+            //вставляем эту медиану
+            medians.emplace(medians.cbegin() + ind + 1, med);
+            //перемещаемся к следующей
+            ++ind;
+        }
+    }
 }
+//сортировка просто многочисленным разделением
+void separation_sort(int* arr, size_t size) {
+    int dlina = size, ind{ 0 };
 
-void sort_merge(int* arr, size_t size)
+    while (ind != dlina - 1) {
+        int left{ ind }, right{ dlina - 1 };
+        //в результате самый наименьший элемент окажется слева
+        while (left != right) {
+            int med = separAndGetMed(arr, left, right);
+            right = med;
+        }
+        //увеличиваем индекс, чтобы потом идти от минимального + 1
+        ++ind;
+    }
+}
+//слияние
+void merge(int* arr1, size_t size1, int *arr2, size_t size2, int *arrRes)
 {
-
+    int dlina1 = size1, dlina2 = size2, ind1{ 0 }, ind2{ 0 }, indRez{0};
+    //пока не вышли за границы одного из массивов
+    while ((ind1 < dlina1) && (ind2 < dlina2))  {
+        //если эл-т какого-то массива меньше эл-та другого массива, 
+        //записываем его и сдвигаем границу этого же массива 
+        if (arr1[ind1] <= arr2[ind2]) {
+            arrRes[indRez] = arr1[ind1];
+            ++ind1;
+        }
+        else {
+            arrRes[indRez] = arr2[ind2];
+            ++ind2;
+        }
+        ++indRez;
+    }
+    //если значения какого-то массива закончились, 
+    //записываем хвост другого массива
+    if (ind1 == dlina1) {
+        for (int i{ind2}; i < dlina2; ++i) {
+            arrRes[indRez] = arr2[i];
+            ++indRez;
+        }
+    }
+    if (ind2 == dlina2) {
+        for (int i{ ind1 }; i < dlina1; ++i) {
+            arrRes[indRez] = arr1[i];
+            ++indRez;
+        }
+    }
+}
+//сортировка слиянием
+void sort_merge(int* arr, int left, int right, int *arrZapas) {
+    int M{ (left + right) / 2 };
+    //спуск
+    if (left < M) {
+        sort_merge(arr, left, M, arrZapas);
+    }
+    if (M + 1 < right) {
+        sort_merge(arr, M + 1, right, arrZapas);
+    }
+    //слияние
+    merge(&arr[left], M - left + 1, &arr[M + 1], right - M, &arrZapas[left]);
+    //копирование
+    for (int i = left; i <= right; i++) {
+        arr[i] = arrZapas[i];
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -184,12 +379,13 @@ int main() {
         if (!size)
         {
             cout << "Array size?" << endl;
-            if (!(cin >> size) || size <= 0 || size > 10000000) throw logic_error("WRONG ARRAY SIZE");
+            if (!(cin >> size) || size <= 0 || size > 1000000000) throw logic_error("WRONG ARRAY SIZE");
 
             arr_unsorted = new int[size];
             srand(unsigned(time(nullptr)));
+
             for (size_t i = 0; i < size; arr_unsorted[i++] = rand() % size);
-            //
+                        
             /*cout << "massiv: ";
             for (int i{0}; i < size; ++i) {
                 cout << arr_unsorted[i] << " ";
@@ -202,18 +398,91 @@ int main() {
             arr_unsorted = new int[5]{ 5, 4, 3, 2, 1 };
         }
 
-
-        //  exec_sort( "Quick nonrecursive:",  sort_quick_nonrec );
-        //  exec_sort( "Quick:",  sort_quick_rec );
-        //  exec_sort( "Heap:",   sort_heap );
-        //  exec_sort( "Shell:",  sort_shell_div2 );
-        //  exec_sort( "Shell:",  sort_shell_pow3 );
-        //  exec_sort( "Merge:",  sort_merge );
-          // exec_sort( arr_unsorted, size, sort_insert_bin, "InsertBin:" );
-           exec_sort( arr_unsorted, size, sort_insert_lin, "InsertLin:" );
-           exec_sort( arr_unsorted, size, sort_select, "Select:" );
-           exec_sort( arr_unsorted, size, sort_shaker, "Shaker:" );
         exec_sort(arr_unsorted, size, sort_bubble, "Bubble:");
+        exec_sort(arr_unsorted, size, sort_shaker, "Shaker:");
+        exec_sort(arr_unsorted, size, sort_select, "Select:");
+        exec_sort( arr_unsorted, size, sort_insert_lin, "InsertLin:" );
+        
+        exec_sort(arr_unsorted, size, sort_shell_div2, "Shell/2:");
+        exec_sort(arr_unsorted, size, sort_shell_pow3, "Shell^3:");
+        exec_sort(arr_unsorted, size, sort_heap, "Heap:");
+        exec_sort(arr_unsorted, size, sort_quick_nonrec, "Quick(nonrec):");
+        exec_sort(arr_unsorted, size, separation_sort, "Separation:");
+        
+        cout << endl << "Quick(rec):" << endl;
+        start_timer();
+        sort_quick_rec(arr_unsorted, size, 0, size - 1);
+        stop_timer();
+        if (!array_is_sorted(arr_unsorted, size)) throw logic_error("FAILURE");
+
+        cout << endl << "Merging:" << endl;
+        int* masCopy = new int[size] {0};
+        start_timer();
+        sort_merge(arr_unsorted, 0, size - 1, masCopy);
+        stop_timer();
+        if (!array_is_sorted(arr_unsorted, size)) throw logic_error("FAILURE");
+
+        //сортировка последовательностей из двух файлов и запись в один
+        int mas1[13]{ 0 }, size1{13};
+        fstream file1("file1.txt");
+        string str{ "" };
+        while (!file1.eof()) {
+            string str1{ "" };
+            file1 >> str1;
+            str += str1 + ' ';
+        }
+        file1.close();
+        int ind{ 0 }, indMas{ 0 };
+
+        while (ind < str.size()) {
+            string subStr{ "" };
+            int step{ 1 };
+            
+            while (str[ind] != ' ') {
+                subStr += str[ind];
+                ++ind;
+            }
+
+            mas1[indMas] = stoi(subStr);
+            ++indMas;
+            ind += step;
+        }
+        
+        int mas2[15]{ 0 }, size2{15};
+        fstream file2("file2.txt");
+        str = "";
+        while (!file2.eof()) {
+            string str1{ "" };
+            file2 >> str1;
+            str += str1 + ' ';
+        }
+        ind = 0, indMas = 0;
+
+        while (ind < str.size()) {
+            string subStr{ "" };
+            int step{ 1 };
+
+            while (str[ind] != ' ') {
+                subStr += str[ind];
+                ++ind;
+            }
+
+            mas2[indMas] = stoi(subStr);
+            ++indMas;
+            ind += step;
+        }
+        file2.close();
+        
+        int masResult[28]{ 0 };
+        merge(mas1, size1, mas2, size2, masResult);
+        if (array_is_sorted(masResult, 28)) {
+            fstream file3("file3.txt", ios::app);
+            for (int i{ 0 }; i < 28; ++i) {
+                file3 << masResult[i];
+                file3 << " ";
+            }
+            file3.close();
+        }
         
         delete[] arr_unsorted;
     }
